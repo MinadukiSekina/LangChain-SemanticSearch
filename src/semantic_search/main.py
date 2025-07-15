3c6bc37e1385ab88d920150c5a9f5e95b5068af3
MinadukiSekina <taroufujimoto075@gmail.com>
2025-07-15 02:23:31 +0000

    feat: add text splitting functionality

--- src/semantic_search/main.py
+++ src/semantic_search/main.py
@@ -1,11 +1,14 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 file_path = "src/semantic_search/example_data/nke-10k-2023.pdf"
 loader = PyPDFLoader(file_path)
 
 docs = loader.load()
 
-print(len(docs))
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200, add_start_index=True
+)
+all_splits = text_splitter.split_documents(docs)
 
-print(f"{docs[0].page_content[:200]}\n")
-print(docs[0].metadata)
+print(len(all_splits))
