8e21df1e88a1979fd3f126a6d869e0fcced04ae5
MinadukiSekina <taroufujimoto075@gmail.com>
2025-07-15 02:32:59 +0000

    feat: add vector generation using Google GenAI

--- src/semantic_search/main.py
+++ src/semantic_search/main.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 file_path = "src/semantic_search/example_data/nke-10k-2023.pdf"
@@ -11,4 +12,11 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 all_splits = text_splitter.split_documents(docs)
 
-print(len(all_splits))
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+vector_1 = embeddings.embed_query(all_splits[0].page_content)
+vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+assert len(vector_1) == len(vector_2)
+print(f"Generated vectors of length {len(vector_1)}\n")
+print(vector_1[:10])
