3f91fccbfec4ee3e9302b51eb1916166214252e9
MinadukiSekina <taroufujimoto075@gmail.com>
2025-07-15 05:41:25 +0000

    feat: implement batch retriever for similarity search

--- src/semantic_search/main.py
+++ src/semantic_search/main.py
@@ -1,7 +1,13 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_core.runnables import chain
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from semantic_search.environment.env_loader import load_env
+
+load_env()
 
 file_path = "src/semantic_search/example_data/nke-10k-2023.pdf"
 loader = PyPDFLoader(file_path)
@@ -19,6 +25,17 @@ vector_store = InMemoryVectorStore(embeddings)
 
 ids = vector_store.add_documents(documents=all_splits)
 
-results = vector_store.similarity_search("How many distribution centers does Nike have in the US?")
 
-print(results[0])
+@chain
+def retriever(query: str) -> list[Document]:
+    return vector_store.similarity_search(query, k=1)
+
+
+results = retriever.batch(
+    [
+        "How many distribution centers does Nike have in the US?",
+        "When was Nike incorporated?",
+    ],
+)
+
+print(results)
